21e033b9952692e692ea9a8a700f479a4677ef22
Philip Cooper <phillycooper@hotmail.com>
2020-05-10 16:37:07 +0100

    Adding htmlhelpers
    Added test project.

---
+++ restlessmedia.Module.Web.Tests/HTMLHelperTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using restlessmedia.Test;
+
+namespace restlessmedia.Module.Web.Tests
+{
+  [TestClass]
+  public class HTMLHelperTests
+  {
+    [TestMethod]
+    public void HasText_returns_true_when_html_contains_text_elements()
+    {
+      string html = "<span>d</span>";
+      HTMLHelper.ContainsInnerText(html).MustBeTrue();
+    }
+
+    [TestMethod]
+    public void HasText_returns_false_when_html_does_not_contain_text_elements()
+    {
+      string html = "<span></span>";
+      HTMLHelper.ContainsInnerText(html).MustBeFalse();
+    }
+
+    [TestMethod]
+    public void HasText_returns_true_when_nested_html_contains_text_elements()
+    {
+      string html = "<span><span>d</span></span>";
+      HTMLHelper.ContainsInnerText(html).MustBeTrue();
+    }
+
+    [TestMethod]
+    public void HasText_returns_true_when_html_with_attributes_contains_text_elements()
+    {
+      string html = "<span><span class=\"text\">d</span></span>";
+      HTMLHelper.ContainsInnerText(html).MustBeTrue();
+    }
+
+    [TestMethod]
+    public void HasText_returns_true_when_html_contains_text_with_self_closing_tags()
+    {
+      string html = "<span class=\"text\">d</span><br />";
+      HTMLHelper.ContainsInnerText(html).MustBeTrue();
+    }
+
+    [TestMethod]
+    public void HasText_returns_true_when_html_with_self_closing_tags_does_not_contain_text()
+    {
+      string html = "<span></span><br />";
+      HTMLHelper.ContainsInnerText(html).MustBeFalse();
+    }
+
+    [TestMethod]
+    public void ToText_returns_text_stripping_out_tags()
+    {
+      string html = "<strong>a test</strong>";
+      HTMLHelper.ToText(html).MustBe("a test");
+    }
+
+    [TestMethod]
+    public void ToText_returns_text_stripping_out_tags_when_nested()
+    {
+      string html = "<strong>a test <span>nested</span></strong>";
+      HTMLHelper.ToText(html).MustBe("a test nested");
+    }
+  }
+}
---
+++ restlessmedia.Module.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("restlessmedia.Module.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("restlessmedia.Module.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc1c1392-2cc6-49a4-b623-4213bbd54fc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
