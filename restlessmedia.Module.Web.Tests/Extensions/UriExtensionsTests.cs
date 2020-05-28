b830b0aa070278056052231c3e2bfce3db9dc12d
Philip Cooper <phillycooper@hotmail.com>
2020-05-28 09:18:02 +0100

    UriExtensions

---
+++ restlessmedia.Module.Web.Tests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using restlessmedia.Test;
+using System;
+using Xunit;
+
+namespace restlessmedia.Module.Web.Tests.Extensions
+{
+  public class UriExtensionsTests
+  {
+    public UriExtensionsTests()
+    {
+      _uriValue = "https://www.restlessmedia.co.uk/files/test.csv";
+      Uri.TryCreate(_uriValue, UriKind.Absolute, out _uri);
+    }
+
+    [Fact]
+    public void FileNameNoExtension()
+    {
+      _uri.FileNameNoExtension().MustBe("test");
+    }
+
+    [Fact]
+    public void FileName()
+    {
+      _uri.FileName().MustBe("test.csv");
+    }
+
+    [Fact]
+    public void Extension()
+    {
+      _uri.Extension().MustBe(".csv");
+    }
+
+    [Fact]
+    public void Extension_string()
+    {
+      _uriValue.Extension().MustBe(".csv");
+    }
+
+    [Fact]
+    public void AddParam()
+    {
+      _uri.AddParam("foo", "bar").ToString().MustBe("https://www.restlessmedia.co.uk/files/test.csv?foo=bar");
+    }
+
+    private readonly Uri _uri;
+
+    private readonly string _uriValue;
+  }
+}
---
+++ src/Extensions/UriExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Specialized;
+using System.IO;
+using System.Web;
+
+namespace System
+{
+  public static class UriExtensions
+  {
+    public static string FileNameNoExtension(this Uri uri)
+    {
+      if (uri == null)
+      {
+        return null;
+      }
+
+      return Path.GetFileNameWithoutExtension(uri.LocalPath);
+    }
+
+    public static string FileName(this Uri uri)
+    {
+      if (uri == null)
+      {
+        return null;
+      }
+
+      return Path.GetFileName(uri.LocalPath);
+    }
+
+    public static string Extension(this Uri uri)
+    {
+      if (uri == null)
+      {
+        return null;
+      }
+
+      return Path.GetExtension(uri.ToString());
+    }
+
+    public static string Extension(this string url)
+    {
+      if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri uri))
+      {
+        return null;
+      }
+
+      return Extension(uri);
+    }
+
+    public static Uri AddParam(this Uri uri, string name, string value)
+    {
+      UriBuilder uriBuilder = new UriBuilder(uri);
+      NameValueCollection paramValues = HttpUtility.ParseQueryString(uriBuilder.Query);
+      paramValues[name] = value;
+      uriBuilder.Query = paramValues.ToString();
+      return uriBuilder.Uri;
+    }
+  }
+}
